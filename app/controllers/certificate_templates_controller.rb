bd09849bacbe9d4d501c9d547fa7e0f10b65a523
yshmarov <yshmarov@gmail.com>
2023-03-11 13:12:58 +0100

    seed and display templates

--- app/controllers/certificate_templates_controller.rb
+++ app/controllers/certificate_templates_controller.rb
@@ -1,2 +1,5 @@
 class CertificateTemplatesController < ApplicationController
+  def index
+    @certificate_templates = CertificateTemplate.all
+  end
 end
--- app/models/certificate_template.rb
+++ app/models/certificate_template.rb
@@ -1,2 +1,3 @@
 class CertificateTemplate < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
